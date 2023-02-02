b83a4c2304c172d064db86aad27b9a1b5cafc957
Joseph Kariuki <josephkariuki790@gmail.com>
2023-02-02 20:04:53 +0300

    added lazy column

--- BasicComposeCodelab/app/src/main/java/com/example/basiccomposecodelab/MainActivity.kt
+++ BasicComposeCodelab/app/src/main/java/com/example/basiccomposecodelab/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.basiccomposecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccomposecodelab.ui.theme.BasicComposeCodelabTheme
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp(modifier = Modifier.fillMaxSize())
                 }
             }
         }
@@ -32,16 +33,86 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+private fun Greeting(name: String) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    Surface(color = MaterialTheme.colorScheme.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)){
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if(expanded.value) "Show Less" else "Show More")
+            }
+        }
+
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyApp(modifier: Modifier= Modifier){
+    var shouldShowOnboarding by remember{mutableStateOf(true)}
+    Surface(modifier) {
+        if (shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = {shouldShowOnboarding=false})
+        }
+        else{
+            Greetings()
+        }
+    }
+}
+@Composable
+private fun Greetings(modifier: Modifier = Modifier,
+names: List<String> = List(1000){"$it"}
+){
+    LazyColumn(modifier= modifier.padding(vertical = 4.dp)){
+        items(items = names){name->
+            Greeting(name = name)
+            
+        }
+    }
+    }
+
+
+@Composable
+fun OnboardingScreen(onContinueClicked:()->Unit, modifier: Modifier = Modifier){
+
+    Column(modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Welcome to the basics codelab")
+        Button(modifier= Modifier.padding(vertical = 24.dp),onClick = onContinueClicked) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    BasicComposeCodelabTheme {
+        //OnboardingScreen(onContinueClicked = )
+    }
+}
+
+@Preview
+@Composable
+fun GreetingsPreview()
+{
+    BasicComposeCodelabTheme {
+        Greetings()
+    }
+}
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicComposeCodelabTheme {
-        Greeting("Android")
+        MyApp(Modifier.fillMaxSize())
     }
 }
